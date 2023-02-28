afb35ffe95515ed731e3e01281cad1b3eb721e87
Comunity blogss <90427022+Comunityblogs@users.noreply.github.com>
2023-02-27 21:40:02 -0500

    Add files via upload

---
+++ js/music-list2.js
@@ -0,0 +1,80 @@
+let allMusic = [
+    {
+      name: "Candy Shop",
+      artist: "The Fritz",
+      img: "music-14",
+      src: "music-14"
+    },
+    {
+      name: "Blessed",
+      artist: "LVDS",
+      img: "music-15",
+      src: "music-15"
+    },
+    {
+      name: "Cafe Cubano",
+      artist: "Amine Maxwell",
+      img: "music-16",
+      src: "music-16"
+    },
+    {
+      name: "Throw It Back",
+      artist: "ProleteR",
+      img: "music-17",
+      src: "music-17"
+    },
+    {
+      name: "Little Doggy",
+      artist: "Prepositiion",
+      img: "music-18",
+      src: "music-18"
+    },
+    {
+      name: "Nightcore - Gangsta's Paradise",
+      artist: "Music For Youtube - Magic Sound",
+      img: "music-19",
+      src: "music-19"
+    },
+    {
+      name: "Lighting Up The Dark",
+      artist: "Music For Youtube - Magic Sound",
+      img: "music-20",
+      src: "music-20"
+    },
+    {
+      name: "Nightcore - Memories ",
+      artist: "Music For Youtube - Magic Sound",
+      img: "music-21",
+      src: "music-21"
+    },
+    {
+      name: "Nightcore - In my dreams",
+      artist: "Music For Youtube - Magic Sound",
+      img: "music-22",
+      src: "music-22"
+    },
+    {
+      name: "Romy Wave - Light Out",
+      artist: "Music For Youtube - Magic Sound",
+      img: "music-23",
+      src: "music-23"
+    },
+    {
+      name: "Imagine Dragons & J.I.D - Enemy",
+      artist: "KingMusic Official",
+      img: "music-24",
+      src: "music-24"
+    },
+    {
+      name: "The Neighbourhood - Sweater Weather",
+      artist: "Remarkable Music",
+      img: "music-25",
+      src: "music-25"
+    },
+    {
+      name: "Sweet Dreams - The Remix Cartell",
+      artist: "The WinFexX - La mejor Musica Sin Copyright",
+      img: "music-26",
+      src: "music-26"
+    },
+  ];
